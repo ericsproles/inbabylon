caf5953833e53206218f1b529e219355b0967e03
ericsproles <tron04@gmail.com>
2020-11-24 17:28:52 -0800

    update server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const AdminBro = require('admin-bro');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const options = require('./admin.options');
 const buildAdminRouter = require('./admin.router');
 
@@ -9,7 +10,7 @@ const port = 3000;
 
 const run = async () => {
   // CONNECT MONGODB ATLAS
-  const connectionString = 'mongodb+srv://inbabylon:inbabylon@cluster0.kutiv.mongodb.net/test?retryWrites=true&w=majority';
+  const connectionString = `mongodb+srv://inbabylon:${process.env.MONGO_PASSWORD}@cluster0.kutiv.mongodb.net/test?retryWrites=true&w=majority`;
   await mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
